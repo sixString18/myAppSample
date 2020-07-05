8cb84a37041b06a5eca164222c61d0f027383e23
Ray Clark <rclarknb@gmail.com>
2020-07-05 10:57:25 -0700

    changed index.html & app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+/* This is a Javascript file */
+
+
+////Event Listner for Form Submit////
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('#div1').innerHTML = "This is a Message!"
+});
